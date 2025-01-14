af1219be0c05f36e27f06d35e88a26b822c8b7a7
xintao <xintao0312@163.com>
2025-01-14 22:23:10 +0800

    25.0114.2

--- src/main/java/xintao/azuretraveller/datagen/AtRecipeProvider.java
+++ src/main/java/xintao/azuretraveller/datagen/AtRecipeProvider.java
@@ -53,6 +53,126 @@ public class AtRecipeProvider extends FabricRecipeProvider
                 .input('#', AtItemTags.SUGAR_REFINING_TAG)
                 .criterion("has_item", RecipeProvider.conditionsFromItem(Items.BEETROOT))
                 .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "beetroot_to_sugar"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.MYTHRIL_SWORD, 1)
+                .pattern(" # ")
+                .pattern(" # ")
+                .pattern(" * ")
+                .input('#', AtItems.MYTHRIL_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.MYTHRIL_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "mythril_sword"));
+        
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.MYTHRIL_PICKAXE, 1)
+                .pattern("###")
+                .pattern(" * ")
+                .pattern(" * ")
+                .input('#', AtItems.MYTHRIL_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.MYTHRIL_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "mythril_pickaxe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.MYTHRIL_AXE, 1)
+                .pattern("## ")
+                .pattern("#* ")
+                .pattern(" * ")
+                .input('#', AtItems.MYTHRIL_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.MYTHRIL_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "mythril_axe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.MYTHRIL_SHOVEL, 1)
+                .pattern(" # ")
+                .pattern(" * ")
+                .pattern(" * ")
+                .input('#', AtItems.MYTHRIL_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.MYTHRIL_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "mythril_shovel"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.MYTHRIL_HOE, 1)
+                .pattern("## ")
+                .pattern(" * ")
+                .pattern(" * ")
+                .input('#', AtItems.MYTHRIL_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.MYTHRIL_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "mythril_hoe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.OBSIDIAN_SWORD, 1)
+                .pattern(" # ")
+                .pattern(" # ")
+                .pattern(" * ")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_sword"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.OBSIDIAN_PICKAXE, 1)
+                .pattern("###")
+                .pattern(" * ")
+                .pattern(" * ")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_pickaxe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.OBSIDIAN_AXE, 1)
+                .pattern("## ")
+                .pattern("#* ")
+                .pattern(" * ")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_axe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.OBSIDIAN_SHOVEL, 1)
+                .pattern(" # ")
+                .pattern(" * ")
+                .pattern(" * ")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_shovel"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, AtItems.OBSIDIAN_HOE, 1)
+                .pattern(" # ")
+                .pattern(" # ")
+                .pattern(" * ")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .input('*', Items.STICK)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_hoe"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.OBSIDIAN_HELMET, 1)
+                .pattern("###")
+                .pattern("# #")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_helmet"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.OBSIDIAN_CHESTPLATE, 1)
+                .pattern("# #")
+                .pattern("###")
+                .pattern("###")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_chestplate"));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.OBSIDIAN_LEGGINGS, 1)
+                .pattern("###")
+                .pattern("# #")
+                .pattern("# #")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_leggings"));
+        
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, AtItems.OBSIDIAN_BOOTS, 1)
+                .pattern("# #")
+                .pattern("# #")
+                .input('#', AtItems.OBSIDIAN_INGOT)
+                .criterion("has_item", RecipeProvider.conditionsFromItem(AtItems.OBSIDIAN_INGOT))
+                .offerTo(exporter, Identifier.of(AzureTraveller.MOD_ID, "obsidian_boots"));
         
         offerSmelting(exporter, MYTHRIL_INGOT, RecipeCategory.MISC, AtItems.MYTHRIL_INGOT, 
                 0.7f, 200, "mythril_ingot");
