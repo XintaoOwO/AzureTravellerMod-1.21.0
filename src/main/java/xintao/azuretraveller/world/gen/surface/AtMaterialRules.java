c9638d77bbc16e7ef6c9313b7ae47fcfbe40c904
xintao <andymc233@163.com>
2025-01-31 21:43:55 +0800

    25.0131.1

--- src/main/java/xintao/azuretraveller/world/gen/surface/AtMaterialRules.java
+++ src/main/java/xintao/azuretraveller/world/gen/surface/AtMaterialRules.java
@@ -3,11 +3,13 @@ package xintao.azuretraveller.world.gen.surface;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+import xintao.azuretraveller.world.biome.AtBiomes;
 
 public class AtMaterialRules 
 {
     private static final MaterialRules.MaterialRule DIRT = materialRule(Blocks.DIRT);
     private static final MaterialRules.MaterialRule GRASS_BLOCK = materialRule(Blocks.GRASS_BLOCK);
+    private static final MaterialRules.MaterialRule STONE = materialRule(Blocks.STONE);
     
     private static MaterialRules.MaterialRule materialRule(Block block)
     {
@@ -20,6 +22,10 @@ public class AtMaterialRules
         MaterialRules.MaterialRule grassBlockSurface = MaterialRules.sequence(MaterialRules.condition(waterLevel, GRASS_BLOCK), DIRT);
         
         return MaterialRules.sequence(
+                MaterialRules.sequence(MaterialRules.condition(MaterialRules.biome(AtBiomes.FRUIT_FOREST), 
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, STONE)), 
+                        MaterialRules.condition(MaterialRules.STONE_DEPTH_CEILING, STONE)), 
+                
                 MaterialRules.condition(MaterialRules.STONE_DEPTH_FLOOR, grassBlockSurface)
         );
     }
