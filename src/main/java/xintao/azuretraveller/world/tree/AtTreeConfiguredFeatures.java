1688c893464d11f3baba44373bb0c5ba032536ae
xintao <andymc233@163.com>
2025-02-01 11:46:07 +0800

    25.0201.1

--- src/main/java/xintao/azuretraveller/world/tree/AtTreeConfiguredFeatures.java
+++ src/main/java/xintao/azuretraveller/world/tree/AtTreeConfiguredFeatures.java
@@ -24,35 +24,38 @@ public class AtTreeConfiguredFeatures
     
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable)
     {
-        register(featureRegisterable, CELESTE_TREE_CONFIGURED, Feature.TREE, new TreeFeatureConfig.Builder(
-                BlockStateProvider.of(AtBlocks.CELESTE_LOG), 
-                new StraightTrunkPlacer(4, 2, 0), 
-                BlockStateProvider.of(AtBlocks.CELESTE_LEAVES),
-                // 半径，偏移量，高度
-                new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(2), 2), 
-                new TwoLayersFeatureSize(0, 0, 0)
-            )
-            .build()
+        register(featureRegisterable, CELESTE_TREE_CONFIGURED, Feature.TREE, 
+                new TreeFeatureConfig.Builder(
+                        BlockStateProvider.of(AtBlocks.CELESTE_LOG), 
+                        new StraightTrunkPlacer(4, 2, 0), 
+                        BlockStateProvider.of(AtBlocks.CELESTE_LEAVES),
+                        // 半径，相对于树干的偏移量，高度
+                        new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), 2), 
+                        new TwoLayersFeatureSize(1, 0, 1)
+                )
+                .build()
         );
         
-        register(featureRegisterable, PEAR_TREE_CONFIGURED, Feature.TREE, new TreeFeatureConfig.Builder(
-                BlockStateProvider.of(AtBlocks.PEAR_LOG), 
-                new StraightTrunkPlacer(4, 1, 0), 
-                BlockStateProvider.of(AtBlocks.PEAR_LEAVES), 
-                new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(2), 2), 
-                new TwoLayersFeatureSize(0, 0, 0)
-            )
-            .build()
+        register(featureRegisterable, PEAR_TREE_CONFIGURED, Feature.TREE, 
+                new TreeFeatureConfig.Builder(
+                        BlockStateProvider.of(AtBlocks.PEAR_LOG), 
+                        new StraightTrunkPlacer(4, 1, 0), 
+                        BlockStateProvider.of(AtBlocks.PEAR_LEAVES), 
+                        new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), 2), 
+                        new TwoLayersFeatureSize(1, 0, 1)
+                )
+                .build()
         );
 
-        register(featureRegisterable, PEACH_TREE_CONFIGURED, Feature.TREE, new TreeFeatureConfig.Builder(
-                BlockStateProvider.of(AtBlocks.PEACH_LOG),
-                new StraightTrunkPlacer(2, 1, 0),
-                BlockStateProvider.of(AtBlocks.PEACH_LEAVES),
-                new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(2), 2), 
-                new TwoLayersFeatureSize(0, 0, 0)
-            )
-            .build()
+        register(featureRegisterable, PEACH_TREE_CONFIGURED, Feature.TREE, 
+                new TreeFeatureConfig.Builder(
+                        BlockStateProvider.of(AtBlocks.PEACH_LOG),
+                        new StraightTrunkPlacer(2, 1, 0),
+                        BlockStateProvider.of(AtBlocks.PEACH_LEAVES),
+                        new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), 2), 
+                        new TwoLayersFeatureSize(1, 0, 1)
+                )
+                .build()
         );
     }
     
