5f9b96ea464b27eb9e5fe823c60dd4111d23a8db
xintao <andymc233@163.com>
2025-04-04 14:41:54 +0800

    25.0404.0

--- src/main/java/xintao/azuretraveller/AzureTraveller.java
+++ src/main/java/xintao/azuretraveller/AzureTraveller.java
@@ -12,13 +12,19 @@ import xintao.azuretraveller.world.gen.AtWorldGeneration;
 
 public class AzureTraveller implements ModInitializer
 {
+	/**
+	 * 这是整个模组的主类，在游戏启动时初始化。
+	 * 模组使用 Java 21，基于 Fabric 和 Minecraft 1.21 开发，兼容 1.21 和 1.21.1 两个版本。
+	 */
+	// 模组ID
 	public static final String MOD_ID = "azuretraveller";
-
+	
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// 主类的初始化方法
 	@Override
 	public void onInitialize()
 	{
